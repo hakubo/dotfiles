0d812f788565ef7c46a6d7de91933594314d6f74
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-07-25 22:56:36 +0200

    lazyload nvm and disalbe some plugins

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,5 +1,23 @@
 if [ "$(uname -s)" = "Darwin" ]
 then
-  export NVM_DIR=~/.nvm
-  . $(brew --prefix nvm)/nvm.sh
+  lazynvm() {
+    unset -f nvm node npm
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+  }
+
+  nvm() {
+    lazynvm
+    nvm $@
+  }
+
+  node() {
+    lazynvm
+    node $@
+  }
+
+  npm() {
+    lazynvm
+    npm $@
+  }
 fi

fd0523e6dfc49adae41d8314f5536eefdaaf00ba
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-08-03 20:28:23 +0200

    composer oh-my-zsh and offenders

---
+++ text/offenders.zsh
@@ -0,0 +1,2 @@
+alias offenders="echo '==='; pbpaste | grep -E -i '([\.\!\?\;]\W*(so|however))|of|brake|surely|easy|everyone knows|just|clearly|of course|simply|basically|obviously|though|some|more|less|bigger|smaller|although|unfortunatelly|any\w*|some\w*|a lot|$'; echo '==='"
+

829c91b9d83b0b1ae4011f36b7241fede568c768
jolek <jolek@egnyte.com>
2019-12-22 13:21:13 +0100

    update

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.cabal/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

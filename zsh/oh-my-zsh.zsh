f835a1c1f39579fe9ecd689ca0fd29fd6039229a
Jakub Olek <jolek@egnyte.com>
2024-04-04 09:53:23 +0200

    updates

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -21,7 +21,7 @@ DISABLE_UNTRACKED_FILES_DIRTY="true"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(command-not-found dircycle git history history-substring-search nvm)
+plugins=(command-not-found dircycle git history history-substring-search nvm z sudo)
 
 # User configuration
 
